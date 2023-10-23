ed702cd81ea82265439979c4efc1e893a548e369
Vinícius <vinicius.campos235silva@gmail.com>
2023-10-23 14:49:48 -0300

    Método PUT

--- src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -12,27 +12,55 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping
 public class UsuarioController {
 
     @Autowired
-    private IUsuarioRepository dao;
+    private IUsuarioRepository usuarioRepository;
 
     @Transactional
-    @GetMapping
+    @GetMapping("usuarios")
     public List<UsuarioModel> listaUsuarios(){
-        return (List<UsuarioModel>) dao.findAll();
+        return (List<UsuarioModel>) usuarioRepository.findAll();
     }
 
     @Transactional
-    @PostMapping
+    @PostMapping("usuarios")
     public ResponseEntity<UsuarioModel> criarUsuario(@RequestBody @Valid UsuarioRecordsDto usuarioRecord){
         UsuarioModel usuarioModel = new UsuarioModel();
         BeanUtils.copyProperties(usuarioRecord, usuarioModel);
-        UsuarioModel usuarioSalvo = dao.save(usuarioModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioSalvo);
+        UsuarioModel usuarioSalvo = usuarioRepository.save(usuarioModel);
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioSalvo);
     }
+
+    @PutMapping("usuarios/{id}")
+    public ResponseEntity<Object> alterarUsuario(@PathVariable(value = "id") Integer id,
+                                                       @RequestBody @Valid UsuarioRecordsDto usuarioRecord){
+        Optional<UsuarioModel> usuari0= usuarioRepository.findById(id);
+        if(usuari0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        var usuarioModel = usuari0.get();
+        BeanUtils.copyProperties(usuarioRecord, usuarioModel);
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuarioModel));
+    }
+
+/*    @PutMapping("/usuarios/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable(value = "id") Integer id,
+                                           @Valid @RequestBody UsuarioRecordsDto usuarioRecord) throws ResourceNotFoundException {
+        UsuarioModel usuarioModel = usuarioRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("User not found for this id :: " + id));
+
+        usuarioModel.setEmail(usuarioModel.getEmail());
+        usuarioModel.setNome_completo(usuarioModel.getLastName());
+        usuarioModel.setUsername(usuarioModel.getFirstName());
+        final User updatedUser = userRepository.save(usuarioModel);
+        return ResponseEntity.ok(updatedUser);
+    }*/
+
+
 
 }
